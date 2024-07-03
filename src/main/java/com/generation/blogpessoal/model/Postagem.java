ec997c3906dcf1f05e54813c20b782c01404b627
FShinoda <fernanda.shinoda@hotmail.com>
2024-07-03 14:09:15 -0300

    feat: postagem get and post done

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,66 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message="O atributo titulo é obrigatorio")
+	@Size(min=5, max=100)
+	private String titulo;
+	
+	@NotBlank(message="O atributo texto é obrigatorio")
+	@Size(min=10, max=1000)
+	private String texto;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	
+
+}
