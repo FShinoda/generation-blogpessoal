1b5dfcabb2bf2e73e82bc5b4c5114b6c2e4217e8
FShinoda <fernanda.shinoda@hotmail.com>
2024-07-04 14:50:00 -0300

    feat: tema class created

--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -4,10 +4,13 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -28,6 +31,18 @@ public class Postagem {
 	
 	@UpdateTimestamp
 	private LocalDateTime data;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 
 	public Long getId() {
 		return id;
--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,6 +1,10 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.blogpessoal.model.Postagem;
@@ -8,5 +12,8 @@ import com.generation.blogpessoal.model.Postagem;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
+	public Optional<Postagem> findByTitulo(String titulo);
+	
+	public List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 	
 }
